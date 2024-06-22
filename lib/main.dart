78bea2d9b69beeeb63457b0f250f9ca1e30a78bf
= <=>
2024-06-22 19:52:19 +0430

    starter code for xylophone tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:xylophone/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,7 +10,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomeScreen(),
+      home: Scaffold(),
     );
   }
 }
